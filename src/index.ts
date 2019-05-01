ce08db7212fa85fbace2d5a29575f47d9a51eaf2
chenyue <285653184@qq.com>
2019-05-01 20:03:59 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@
 // const Router = require('koa-router')
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
-import Hello from './say'
 
 const app = new Koa()
 const router = new Router()
 
 console.log(process.env.PORT, process.env.NODE_ENV)
 router.get('/*', async (ctx) => {
-  ctx.body = 'Hellow World !' + Hello(1, 1)
+  ctx.body = 'Hellow World !'
 })
 
 app.use(router.routes())
